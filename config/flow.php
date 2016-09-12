a8f2a8e82ee82ec1772576957fd4213de0720f64
Eddie Lau <277676978@qq.com>
2016-09-12 11:27:39 +0800

    drop 'yuanhui' config-item.

--- config/flow.php
+++ config/flow.php
@@ -59,24 +59,4 @@ return [
         'appSecret' => ''
     ],
 
-    /*
-     * 源慧
-     */
-    'yuanhui' => [
-        /*
-         * 客户/账号
-         */
-        'cid' => 107,
-
-        /*
-         * 获取 API 校验
-         */
-        'appkey' => 'DEMO_Pass_Edrt%Rt',
-
-        /*
-         * 服务地址
-         */
-        'url' => 'http://demo.eswapi.com/API/',
-    ],
-
 ];
