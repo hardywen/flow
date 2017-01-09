cf1b1a7cff87ecd796d88129602f3cacf3971086
hardywen <375480616@qq.com>
2017-01-09 10:26:40 +0800

    add lumen provider

---
+++ src/FlowServiceProviderForLumen.php
@@ -0,0 +1,38 @@
+<?php
+namespace Hardywen\Flow;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class FlowServiceProviderForLumen extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('flow', function ($app) {
+            return new FlowManager($app);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['flow'];
+    }
+}
